8f98b67f86fd965daeff9290dfb6b29e657a094c
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-11-11 10:25:33 -0300

    :pencil: Exercites.

--- Operations1111/Main.java
+++ Operations1111/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
         Operations operations = new Operations();
         // operations.enterNumbers();
-        operations.addition(5, 5);
-        operations.subtraction(5, 5);
-        operations.multiplication(5, 5);
-        operations.division(5, 5);
-        operations.showResults(5, 5);
+        int add = operations.addition(5, 5);
+        int subtract = operations.subtraction(5, 5);
+        int multiply = operations.multiplication(5, 5);
+        int divide = operations.division(5, 5);
+        operations.showResults(5, 5, add, subtract, multiply, divide);
 
     }
 
--- Operations1111/Operations.java
+++ Operations1111/Operations.java
@@ -7,10 +7,10 @@ public class Operations {
     //Attributes
     // int number1;
     // int number2;
-    int add;
-    int subtract;
-    int multiply;
-    int divide;
+    // int add;
+    // int subtract;
+    // int multiply;
+    // int divide;
 
     //Methods
 
@@ -28,28 +28,32 @@ public class Operations {
     // }
 
     //Addition
-    public void addition(int number1, int number2) {
-        add = number1 + number2;
+    public int addition(int number1, int number2) {
+        int add = number1 + number2;
+        return add;
     }
 
     //Subtraction
-    public void subtraction(int number1, int number2) {
-        subtract = number1 - number2;
+    public int subtraction(int number1, int number2) {
+        int subtract = number1 - number2;
+        return subtract;
     }
 
     //Multiplication
-    public void multiplication( int number1, int number2) {
-        multiply = number1 * number2;
+    public int multiplication( int number1, int number2) {
+        int multiply = number1 * number2;
+        return multiply;
     }
 
     //Division
-    public void division(int number1, int number2) {
-        divide = number1 / number2;
+    public int division(int number1, int number2) {
+        int divide = number1 / number2;
+        return divide;
     }
 
     //Show results
-    public void showResults( int number1, int number2) {
-        System.out.println("The addition of " + number1 + " and " + number2 + " is " + add);
+    public void showResults( int number1, int number2, int add, int subtract, int multiply, int divide) {
+        System.out.println("The addition of " + number1 + " and " + number2 + " is " + add );
         System.out.println("The subtraction of " + number1 + " and " + number2 + " is " + subtract);
         System.out.println("The multiplication of " + number1 + " and " + number2 + " is " + multiply);
         System.out.println("The division of " + number1 + " and " + number2 + " is " + divide);
