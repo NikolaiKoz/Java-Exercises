00cd4aeb475248b4c45968b99240fd75401a3ff1
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-11-11 10:47:54 -0300

    :pencil: Object Person.

---
+++ SesentayCinco/Main.java
@@ -0,0 +1,16 @@
+package SesentayCinco;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Persona persona1 = new Persona("Juan", 33);
+        Persona persona2 = new Persona("Pedro", 25);
+
+        System.out.println("persona1 name: " + persona1.name);
+        System.out.println("persona1 age: " + persona1.age);
+        System.out.println("persona2 name: " + persona2.name);
+        System.out.println("persona2 age: " + persona2.age);
+
+
+    }
+}
---
+++ SesentayCinco/Persona.java
@@ -0,0 +1,20 @@
+package SesentayCinco;
+
+public class Persona {
+
+    //Atributes
+    String name;
+    int age;
+
+    //Constructor
+    public Persona(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+
+
+
+}
